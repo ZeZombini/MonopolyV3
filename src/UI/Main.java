777f92c348e1590ed38ad3604242e1efc751d607
zombini <zombini@Zombinux>
2015-06-11 19:02:14 +0200

    Creation du main

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -6,7 +6,16 @@ import Jeu.Monopoly;
 public class Main {
 
 	public static void main(String[] args) {
-		Monopoly m = new Monopoly("/home/georges/workspace/Monopoly/data/case.data");
+		Monopoly m = new Monopoly("src//Data//data.txt");
+                Interface inter = new Interface(m);
+                m.setInter(inter);
+                inter.initialiserPartie();
+                while (m.getJoueurs().size() != 1) {
+                    m.jouer();
+                }
+                
+                System.out.println("Bravo ! Partie finit !");
+                System.out.println("Joueur " + m.getJoueurs().get(0).getNomJoueur() + " a gagné !");
 	}
 
 }
