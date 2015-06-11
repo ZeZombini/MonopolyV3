4cde5187b0b03257dc89567693e387cf183f3bc7
zombini <zombini@Zombinux>
2015-06-11 08:57:10 +0200

    Suppression d'une valeur inutile

--- src/Jeu/CarteChanceEnum.java
+++ src/Jeu/CarteChanceEnum.java
@@ -1,5 +1,5 @@
 package Jeu;
 
 public enum CarteChanceEnum {
-	avancez_depart, libere_prison, payez_reparations_voirie, payez_reparations_maison, payez_amende_vitesse, payez_amende_ivresse, payez_scolarite, recevez_prix_mots_croises, recevez_banque_dividende, recevez_immeuble_prêt, allez_prison, reculez_3cases, recevez_revenu_annuel, avancez_rue_paix, avancez_henri_martin, avancez_gare_lyon, avancez_boulevard_villette;
+	avancez_depart, libere_prison, payez_reparations_voirie, payez_reparations_maison, payez_amende_vitesse, payez_amende_ivresse, payez_scolarite, recevez_prix_mots_croises, recevez_banque_dividende, recevez_immeuble_prêt, allez_prison, reculez_3cases, avancez_rue_paix, avancez_henri_martin, avancez_gare_lyon, avancez_boulevard_villette;
 }
